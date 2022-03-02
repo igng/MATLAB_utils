d00088a5ff37678756c821fcb04e7898dd8e46e8
Andrea Monti <monti.d.andrea@gmail.com>
2022-03-02 14:05:10 +0100

    Fixed script_name with .m extension

--- from_template.m
+++ from_template.m
@@ -1,4 +1,10 @@
 function [] = from_template(script_name)
-    copyfile("~/Uni/PhD/MATLAB_utils/matlab_template.m", strcat(script_name, ".m"));
-    edit(script_name);
+    if (script_name{1}(end-1:end) == ".m")
+        filename = script_name{1}(1:end-2);
+        copyfile("~/Uni/PhD/MATLAB_utils/matlab_template.m", script_name);
+    else
+        filename = script_name;
+        copyfile("~/Uni/PhD/MATLAB_utils/matlab_template.m", strcat(script_name, ".m"));
+    end
+    edit(filename);
 end
